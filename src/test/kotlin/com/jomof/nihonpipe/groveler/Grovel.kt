602859b5de3b0a09592a3de86bcbb826a72a2827
jomof <jomofisher@gmail.com>
2018-02-24 16:02:46 -0800

    Partial tanaka

--- src/test/kotlin/com/jomof/nihonpipe/groveler/Grovel.kt
+++ src/test/kotlin/com/jomof/nihonpipe/groveler/Grovel.kt
@@ -64,7 +64,7 @@ class Grovel {
         }
         jacyDataTanakaDir.walkTopDown()
                 .toList()
-                // .take(2)
+                .take(5)
                 .forEach { file ->
                     if (file.isFile) {
                         translateTanakaCorpus(file)
